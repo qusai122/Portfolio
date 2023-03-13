03fcdfd38f4ef3d942ac835c259f03b62bafa683
Qusai Issa <qusai.2010.123@hotmail.com>
2023-03-13 20:46:41 +0200

    Fix Webhint and ESLint errors

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 function ReadElement(selector) {
     return document.querySelector(selector);
-  }
+}
+const menu = ReadElement('#menu-icon');
+const nav = ReadElement('.mobile-nav');
+const closeNav = ReadElement('.close-button');
+const choose = ReadElement('.mobile-nav');
   
-  const menu = ReadElement('#menu-icon');
-  const nav = ReadElement('.mobile-nav');
-  const closeNav = ReadElement('.close-button');
-  const choose = ReadElement('.mobile-nav');
-  
-  function AddMenu(selected) {
-    return selected.addEventListener('click', () => nav.classList.add('nav-toggle'));
-  }
-  
-  function RemoveMenu(selected) {
-    return selected.addEventListener('click', () => nav.classList.remove('nav-toggle'));
-  }
-  
-  AddMenu(menu);
-  RemoveMenu(closeNav);
-  RemoveMenu(choose);
+function AddMenu(selected) {
+  return selected.addEventListener('click', () => nav.classList.add('nav-toggle'));
+}
+function RemoveMenu(selected) {
+  return selected.addEventListener('click', () => nav.classList.remove('nav-toggle'));
+}
+AddMenu(menu);
+RemoveMenu(closeNav);
+RemoveMenu(choose);
