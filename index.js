994bf76c2dfd4a7c3c264a57d352b8c828c20432
Qusai Issa <qusai.2010.123@hotmail.com>
2023-03-13 20:47:55 +0200

    Fix Webhint and ESLint errors 2

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 function ReadElement(selector) {
-    return document.querySelector(selector);
+  return document.querySelector(selector);
 }
 const menu = ReadElement('#menu-icon');
 const nav = ReadElement('.mobile-nav');
 const closeNav = ReadElement('.close-button');
-const choose = ReadElement('.mobile-nav');
-  
+const choose = ReadElement('.mobile-nav'); 
 function AddMenu(selected) {
   return selected.addEventListener('click', () => nav.classList.add('nav-toggle'));
 }
