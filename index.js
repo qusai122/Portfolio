b5dc7a4a1e5711376c99521bc27739b3a4b0d8a8
Qusai Issa <qusai.2010.123@hotmail.com>
2023-03-13 21:15:54 +0200

    Fix JS linters

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function ReadElement(selector) {
 const menu = ReadElement('#menu-icon');
 const nav = ReadElement('.mobile-nav');
 const closeNav = ReadElement('.close-button');
-const choose = ReadElement('.mobile-nav'); 
+const choose = ReadElement('.mobile-nav');
 function AddMenu(selected) {
   return selected.addEventListener('click', () => nav.classList.add('nav-toggle'));
 }
