127770110c24776e0fbd83d9ffc7b355184adb6d
qusai122 <qusai.2010.123@gmail.com>
2023-03-14 15:01:52 +0200

    Fix linters

--- index.js
+++ index.js
@@ -5,19 +5,18 @@ const menu = ReadElement('#menu-icon');
 const nav = ReadElement('.mobile-nav');
 const closeNav = ReadElement('.close-button');
 const choose = ReadElement('.mobile-nav');
+function Add() {
+  nav.classList.add('nav-toggle');
+  document.body.classList.add('stop-scrolling');}
 function AddMenu(selected) {
   return selected.addEventListener('click',Add);
 }
-function Add(){
-  nav.classList.add('nav-toggle');
-  document.body.classList.add('stop-scrolling');
+function Remove() {
+  nav.classList.remove('nav-toggle');
+  document.body.classList.remove('stop-scrolling');
 }
 function RemoveMenu(selected) {
   return selected.addEventListener('click', Remove);
-}
-function Remove(){
-  nav.classList.remove('nav-toggle');
-  document.body.classList.remove('stop-scrolling');
 }
 AddMenu(menu);
 RemoveMenu(closeNav);
