634bafbb0f1982268ae6a5e5f785e683cc82fdc1
Qusai Issa <qusai.2010.123@hotmail.com>
2023-03-14 03:03:28 +0200

    Fix style

--- index.js
+++ index.js
@@ -4,27 +4,24 @@ function ReadElement(selector) {
 const menu = ReadElement('#menu-icon');
 const nav = ReadElement('.mobile-nav');
 const closeNav = ReadElement('.close-button');
-const choose = ReadElement('.mobile-nav');
-function AddMenu(selected) {
-  return selected.addEventListener('click', () => nav.classList.add('nav-toggle'));
-}
-function RemoveMenu(selected) {
-  return selected.addEventListener('click', () => nav.classList.remove('nav-toggle'));
-}
-AddMenu(menu);
-RemoveMenu(closeNav);
-RemoveMenu(choose);
+const CardModal = ReadElement('.modal-section');
 const viewModal1 = ReadElement('.view-card-1');
 const viewModal2 = ReadElement('.view-card-2');
 const viewModal3 = ReadElement('.view-card-3');
 const viewModal4 = ReadElement('.view-card-4');
+
+function RemoveMenu(selected, modalPart, remover) {
+  return selected.addEventListener('click', () => modalPart.classList.remove(remover));
+}
+
+
 const cardDetails = [
   {
     title: 'Tonic',
     name: 'CANOPY',
     technology: 'Back End Dev',
     year: '2025',
-    imageUrl: 'images/Snapshoot-Portfolio.png',
+    imageUrl: 'assits/Snapshoot-Portfolio.png',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     languages: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstraps'],
     liveLink: 'https://www.linkedin.com/in/qusai-issa-ab5a59156/',
@@ -34,7 +31,7 @@ const cardDetails = [
     name: 'CANOPY',
     technology: 'Back End Dev',
     year: '2025',
-    imageUrl: 'images/Snapshoot-Portfolio4.png',
+    imageUrl: 'assits/Snapshoot-Portfolio2.png',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. ry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     languages: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstraps'],
     liveLink: 'https://www.linkedin.com/in/qusai-issa-ab5a59156/',
@@ -44,7 +41,7 @@ const cardDetails = [
     name: 'CANOPY',
     technology: 'Back End Dev',
     year: '2025',
-    imageUrl: 'images/Snapshoot-Portfolio2.png',
+    imageUrl: 'assits/Snapshoot-Portfolio3.png',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     languages: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstraps'],
     liveLink: 'https://www.linkedin.com/in/qusai-issa-ab5a59156/',
@@ -54,7 +51,7 @@ const cardDetails = [
     name: 'CANOPY',
     technology: 'Back End Dev',
     year: '2025',
-    imageUrl: 'images/Snapshoot-Portfolio3.png',
+    imageUrl: 'assits/Snapshoot-Portfolio4.png',
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     languages: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstraps'],
     liveLink: 'https://www.linkedin.com/in/qusai-issa-ab5a59156/',
@@ -62,37 +59,38 @@ const cardDetails = [
   },
 ];
 function renderModal(number) {
-  const card = details[number];
+  const card = cardDetails[number];
   const modalInstance = ` <div class=" card-modal">
       <div class="card-modal-head">
-          <h1 class="title card-modal-head">${card.title}</h1>
+          <h1 class="card-title card-modal-head">${card.title}</h1>
           <span class="close-modal">x</span>
       </div>
-      <div class="position card-modal-head-body">
-          <h4 class="place">${card.name}</h4>
-          <H5 class="post-name">&#x25cf; ${card.technology}</H5>
-          <H5 class="post-year">&#x25cf; ${card.year}</H5>
+      <div class="position card-desc">
+          <h4 class="client-text">${card.name}</h4>
+          <H5 class="role-text">&#x25cf; ${card.technology}</H5>
+          <H5 class="year-text">&#x25cf; ${card.year}</H5>
       </div>
       <img class="card-modal-img" src=${card.imageUrl} alt="The Snapshoot-Portfolio image">
       <div class="card-modal-middle">
-      <p class="description">
+      <div class="card-modal-middle-group1"> <p class="card-info">
       ${card.description}
           </p>
-          <div class="card-modal-middle-group">
+          </div>
+          <div class="card-modal-middle-group2">
               <div class="techs" id="techs">
-                  ${card.languages.map((lang) => `<div class="single-tech" id="techs-item">${lang}</div>`).join('')}
+                  ${card.languages.map((lang) => `<div class="tag-item" id="techs-item">${lang}</div>`).join('')}
                 
                   </div>
                   <hr class="card-modal-hr">
                   <div class="card-modal-btn-grp">
-                  <a href=${card.liveLink} id="btn-link" ><button type="button" class="view-more card-modal-btn">See live <img class="btn-icon" src="images/icons/Icon.png" alt="icon img"></button></a>
-                  <a href=${card.sourceCode} id="btn-link" <button type="button" class="view-more card-modal-btn">See Source <img class="btn-icon" src="images/icons/Vector.png" alt="icon img"></button></a>
+                  <a href=${card.liveLink} id="btn-link" <button type="button" class="view-more card-modal-btn">See live <img class="btn-icon" src="assits/card-icon1.png" alt="icon img"></button></a>
+                  <a href=${card.sourceCode} id="btn-link" <button type="button" class="view-more card-modal-btn">See Source <img class="btn-icon" src="assits/card-icon2.png" alt="icon img"></button></a>
                   </div>
                   </div> 
                   </div>
     </div>`;
 
-  WorkModal.innerHTML = modalInstance;
+  CardModal.innerHTML = modalInstance;
 }
 function AddMenu(selected, modalPart, opener, number) {
   return selected.addEventListener('click', () => {
@@ -104,8 +102,11 @@ function AddMenu(selected, modalPart, opener, number) {
     }
   });
 }
-AddMenu(viewModal1, WorkModal, 'nav-toggle', 1);
-AddMenu(viewModal2, WorkModal, 'nav-toggle', 2);
-AddMenu(viewModal3, WorkModal, 'nav-toggle', 3);
-AddMenu(viewModal4, WorkModal, 'nav-toggle', 4);
+AddMenu(menu, nav, 'nav-toggle');
+AddMenu(viewModal1, CardModal, 'nav-toggle', 1);
+AddMenu(viewModal2, CardModal, 'nav-toggle', 2);
+AddMenu(viewModal3, CardModal, 'nav-toggle', 3);
+AddMenu(viewModal4, CardModal, 'nav-toggle', 4);
+RemoveMenu(CardModal, CardModal, 'nav-toggle');
+RemoveMenu(closeNav, nav, 'nav-toggle');
 RemoveMenu(nav, nav, 'nav-toggle');
