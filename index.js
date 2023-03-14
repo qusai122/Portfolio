c8602161ef3c4eb8b0be9f295158b1e09ba30675
qusai122 <qusai.2010.123@gmail.com>
2023-03-14 15:42:03 +0200

    Adding card data to JS

--- index.js
+++ index.js
@@ -22,3 +22,46 @@ function RemoveMenu(selected) {
 AddMenu(menu);
 RemoveMenu(closeNav);
 RemoveMenu(choose);
+const cardDetails = [
+  {
+    title: 'Tonic',
+    name: 'CANOPY',
+    technology: 'Back End Dev',
+    year: '2025',
+    imageUrl: 'assits/Snapshoot-Portfolio.png',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    languages: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstraps'],
+    liveLink: 'https://www.linkedin.com/in/qusai-issa-ab5a59156/',
+    sourceCode: 'https://github.com/qusai122',
+  }, {
+    title: 'Multi-Post Stories',
+    name: 'CANOPY',
+    technology: 'Back End Dev',
+    year: '2025',
+    imageUrl: 'assits/Snapshoot-Portfolio2.png',
+    description: "A daily selection of privately personalized reads; no accounts or sign-ups required. ry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    languages: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstraps'],
+    liveLink: 'https://www.linkedin.com/in/qusai-issa-ab5a59156/',
+    sourceCode: 'https://github.com/qusai122',
+  }, {
+    title: 'Tonic',
+    name: 'CANOPY',
+    technology: 'Back End Dev',
+    year: '2025',
+    imageUrl: 'assits/Snapshoot-Portfolio3.png',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    languages: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstraps'],
+    liveLink: 'https://www.linkedin.com/in/qusai-issa-ab5a59156/',
+    sourceCode: 'https://github.com/qusai122',
+  }, {
+    title: 'Multi-Post Stories',
+    name: 'CANOPY',
+    technology: 'Back End Dev',
+    year: '2025',
+    imageUrl: 'assits/Snapshoot-Portfolio4.png',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    languages: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstraps'],
+    liveLink: 'https://www.linkedin.com/in/qusai-issa-ab5a59156/',
+    sourceCode: 'https://github.com/qusai122',
+  },
+];
